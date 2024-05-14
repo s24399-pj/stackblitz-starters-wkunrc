173c54cb7e961d284571a828a85ab0151c27ee93
s24399-pj <92204687+s24399-pj@users.noreply.github.com>
2024-05-14 15:13:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-wkunrc
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/s24399-pj/stackblitz-starters-wkunrc)
---
+++ server.js
@@ -0,0 +1,8 @@
+// //Utwórz serwer HTTP w Node.js, który obsługuje pliki statyczne oraz dynamicznie generuje treści.
+// Implementuj serwer w server.js, który używa modułów http i fs.
+// Serwer powinien zwracać index.html jako stronę główną.
+// Obsłuż żądania do plików CSS i JS z katalogu public, który powinien znajdować się w tym samym miejscu co odpalany server
+// Dla ścieżki /dynamic, generuj dynamicznie treść HTML z bieżącą datą i czasem.
+// Obsługa plików statycznych: Zaimplementuj funkcję serveStaticFile do obsługi plików statycznych z odpowiednim MIME type.
+
+module.exports = server;
